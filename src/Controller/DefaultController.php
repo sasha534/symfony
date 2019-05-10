91b04136919b7b2010752ef285c7eefb74555207
Олександр Сорокін <o.sorokin@corp.arifru.dream.net.ua>
2019-05-10 16:20:51 +0300

    added new var number DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -24,6 +24,7 @@ class DefaultController extends AbstractController
 
         return $this->render('index.html.twig',
             ['base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+              'number' => $number
             ]);
     }
 }
